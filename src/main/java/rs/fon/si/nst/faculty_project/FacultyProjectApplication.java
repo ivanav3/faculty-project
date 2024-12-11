3311162b5f674bc22bd501192fa640ae2fc77a34
Ivana Vukmirov <ivanav32001@gmail.com>
2024-12-11 17:22:29 +0100

    Initial commit

---
+++ src/main/java/rs/fon/si/nst/faculty_project/FacultyProjectApplication.java
@@ -0,0 +1,13 @@
+package rs.fon.si.nst.faculty_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/fon/si/nst/faculty_project/FacultyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.fon.si.nst.faculty_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
